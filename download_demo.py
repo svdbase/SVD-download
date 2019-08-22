0d7ea83e544f76f5d2a353ad92adb3c55ad094b9
jiangqy <jiangqy@lamda.nju.edu.cn>
2019-08-22 09:23:21 +0800

    initial comment

---
+++ download_demo.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# !/user/bin/python3
+# +++++++++++++++++++++++++++++++++++++++++++++++++++
+# @File Name: download_demo.py
+# @Author: Jiang.QY
+# @Mail: qyjiang24@gmail.com
+# @Date: 19-8-22
+# +++++++++++++++++++++++++++++++++++++++++++++++++++
+
